25872cbca97ed7ecdf45de65f6458e52f9615b8b
Warren Weckesser <warren.weckesser@gmail.com>
2025-10-12 17:44:30 -0400

    Use checkit.h for unit tests.

--- tests/test_argmin.cpp
+++ tests/test_argmin.cpp
@@ -1,33 +1,27 @@
-#include <cstdio>
+//#include <cstdio>
+#include <iostream>
 #include <vector>
-
+#include "checkit.h"
 #include "xsimd_funcs/minmax.hpp"
 
 namespace xsfun = xsimd_funcs;
 
 int main()
 {
+    auto test = CheckIt(std::cerr);
+
     std::vector<short> v;
 
-    constexpr std::size_t n = 323457;
+    constexpr std::size_t n = 23457;
     for (std::size_t i = 0; i < n; ++i) {
         v.push_back(static_cast<short>(i % 113));
     }
-    int bad_count = 0;
     for (size_t k = 0; k < v.size(); ++k) {
         short tmp = v[k];
         v[k] = -99;
         xsfun::value_index_pair<short> result = xsfun::min_argmin(v);
-        if (result.index != k) {
-            printf("*** incorrect result: k = %zu  result.index = %zu\n", k, result.index);
-            bad_count += 1;
-            if (bad_count >= 5) {
-                break;
-            }
-        }
+        assert_equal_integer(test, result.index, k, "incorrect argmin");
         v[k] = tmp;
     }
-    if (bad_count == 0) {
-        printf("All tests passed.\n");
-    }
+    return test.print_summary("Summary: ");
 }
