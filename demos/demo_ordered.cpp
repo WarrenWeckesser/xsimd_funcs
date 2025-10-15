057ed89ea11a7d2015b15f1ac1055fda8b5978f0
Warren Weckesser <warren.weckesser@gmail.com>
2025-10-14 21:07:07 -0400

    Fix use of renamed function in demo_ordered.cpp.

--- demos/demo_ordered.cpp
+++ demos/demo_ordered.cpp
@@ -9,7 +9,7 @@ namespace xsfun = xsimd_funcs;
 int main()
 {
     std::vector<long> x{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10};
-    auto incr1 = xsfun::is_increasing_v1(x);
+    auto incr1 = xsfun::is_increasing(x);
     std::cout << "x is increasing? v1: " << incr1 << std::endl;
     auto incr2 = xsfun::is_increasing_v2(x);
     std::cout << "x is increasing? v2: " << incr2 << std::endl;
